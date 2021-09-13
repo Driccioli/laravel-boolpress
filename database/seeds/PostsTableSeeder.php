e5dd68ed4ad52bff4ceda8d159816f5d3ad59099
Driccioli <driccioli53@gmail.com>
2021-09-13 17:37:49 +0200

    Migration, seeding

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Post;
+require_once "vendor/autoload.php";
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i=0;$i<50;$i++){
+            $newPost = new Post();
+            $newPost->title = $faker->sentence(3);
+            $newPost->user = $faker->userName();
+            $newPost->userpic= $faker->imageUrl(64, 64, "animals", true);
+            $newPost->content = $faker->paragraphs(3, true);
+        }
+    }
+}
