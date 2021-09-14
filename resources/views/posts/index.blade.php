917718d0ad49b6cc81191243812f2e2fdc59ec2f
Driccioli <driccioli53@gmail.com>
2021-09-14 16:48:53 +0200

    Resource, Index, Show

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+    
+    <div class="container">
+        <table>
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Date posted</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">User</th>
+                    <th scope="col">User pfp</th>
+                    <th scope="col">Content</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($posts as $post)
+                    <tr>
+                        <th scope="row">{{$post->id}}</th>
+                        <td>{{$post->created_at}}</td>
+                        <td>{{$post->title}}</td>
+                        <td>{{$post->user}}</td>
+                        <td><img src="{{$post->userpic}}" alt="{{$post->user}} pfp"></td>
+                        <td>{{$post->content}}</td>
+                        <td><a href="{{route('posts.show', $post->id)}}"><i class="bi bi-zoom-in"></i></a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+@endsection
