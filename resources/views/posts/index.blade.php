68d41946ce339d6d4634c1c00c5f63f4658f9fed
Driccioli <driccioli53@gmail.com>
2021-09-17 17:30:51 +0200

    Tried adding modal

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,6 +3,7 @@
 
 @section('content')
     
+
     <div class="container">
         <a class="" href="{{route('posts.create')}}">Create Post</a>
         <table class="table">
@@ -29,11 +30,39 @@
                         <td>
                             <a class="icon" href="{{route('posts.show', $post->id)}}"><i class="bi bi-zoom-in"></i></a>
                             <a class="icon" href="{{route('posts.edit', $post)}}"><i class="bi bi-pencil-square"></i></a>
-                            <form action="{{route('posts.destroy', $post)}}" method="post">
+                            {{-- <form action="{{route('posts.destroy', $post)}}" method="post">
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" class="icon"><i class="bi bi-trash"></i></button>
-                            </form>
+                            </form> --}}
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                <i class="bi bi-trash"></i>
+                            </button>
+                            
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Deleting post...</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                    Are you sure you want to delete this post?
+                                    </div>
+                                    <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">WAIT NO-</button>
+                                    <form action="{{route('posts.destroy', $post)}}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-primary">I'm sure. Delete it!</button>
+                                    </form>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+  
                         </td>
                     </tr>
                 @endforeach
